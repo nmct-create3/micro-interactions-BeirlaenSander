4a3a99d279665ceddf1990b2dd475892e3c9d451
Seeeender <sander.beirlaen@student.howest.be>
2022-10-06 15:48:14 +0200

    password toggle

--- script/app.js
+++ script/app.js
@@ -1,13 +1,18 @@
-function handleFloatingLabel() {
-   
-}
+function handleFloatingLabel() {}
 
 function handlePasswordSwitcher() {
-    
+  const passwordToggle = document.querySelector('.js-password-toggle');
+  const passwordInput = document.querySelector('.js-password-input');
+
+  const passwordOptions = ['password', 'text'];
+
+  passwordToggle.addEventListener('change', function () {
+    passwordInput.type = passwordOptions[+this.checked];
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Script loaded!');
-    handleFloatingLabel();
-    handlePasswordSwitcher();
+  console.log('Script loaded!');
+  handleFloatingLabel();
+  handlePasswordSwitcher();
 });
